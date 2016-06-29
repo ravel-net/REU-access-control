9198720e93467f2ab7c91e0468a34c14f048d958
nglaeser <nglaeser@email.sc.edu>
2016-06-29 12:48:40 -0500

    Add working trigger to rm_tenant to prevent disallowed insertionsgit add sla_start.sql git add sla_start.sql

--- sla_start.sql
+++ sla_start.sql
@@ -21,6 +21,9 @@ CREATE TABLE sla (name varchar, nodeid integer);
 INSERT INTO sla (name, nodeid) VALUES ('alice', 1), ('alice', 2), ('alice', 3), ('alice', 11), ('alice', 12), ('alice', 13);
 INSERT INTO sla (name, nodeid) VALUES ('bob', 4), ('bob', 5), ('bob', 14), ('bob', 15);
 
+GRANT SELECT ON sla TO PUBLIC;
+/* TODO: fix this security breach */
+
 /* add a column to track who 'owns' each host to the hosts table */
 UPDATE hosts SET tenant = sla.name FROM sla
      WHERE hosts.hid = sla.nodeid;
@@ -46,14 +49,70 @@ CREATE OR REPLACE VIEW topology_tenant AS (
 
 GRANT SELECT ON topology_tenant TO PUBLIC;
 
+CREATE OR REPLACE VIEW sla_tenant AS (
+    SELECT name, nodeid FROM sla
+    WHERE name = current_user );
+GRANT SELECT ON sla_tenant TO PUBLIC;
+
 DROP TABLE IF EXISTS config_sla CASCADE;
 
 /* whitelist for who can talk to whom */
 CREATE TABLE config_sla (p1 varchar, p2 varchar);
 INSERT INTO config_sla (p1, p2) VALUES ('alice', 'bob'), ('alice', 'charlie'), ('bob', 'alice'), ('charlie', 'alice');
 
-/* add some flows to the network */
-INSERT INTO rm (fid, src, dst) VALUES (1, 1, 5), (2, 4, 12), (3, 2, 17), (4, 7, 3), (5, 1, 13);
+
+/* veiws created because triggers do not support subqueries */
+CREATE VIEW rm_trigger_src AS (
+    SELECT nodeid FROM sla WHERE name IN (
+        SELECT p1 FROM config_sla WHERE p2=current_user )
+    UNION
+    SELECT nodeid FROM sla WHERE name = current_user );
+/*this grant is fine because it only shows info the current user knows anyway */
+GRANT SELECT ON rm_trigger_src TO PUBLIC;
+
+CREATE VIEW rm_trigger_dst AS (
+    SELECT nodeid FROM sla WHERE name IN (
+        SELECT p2 FROM config_sla WHERE p1=current_user )
+    UNION
+    SELECT nodeid FROM sla WHERE name = current_user );
+/* this grant is also fine (see rm_trigger_src grant) */
+GRANT SELECT ON rm_trigger_dst TO PUBLIC;
+
+DROP TABLE IF EXISTS rm_exceptions;
+CREATE TABLE rm_exceptions ( name varchar );
+INSERT INTO rm_exceptions (name) VALUES ('ravel');
+
+/* trigger uses AFTER INSERT because WHEN is not supported for INSTEAD OF triggers */
+/* use before trigger (be more general for complicated cases/views that aren't updatabale,
+performance is better to avoid unnecessary actions 
+modify/override postgres modification commands */
+DROP TRIGGER IF EXISTS rm_modifications_trigger ON rm;
+CREATE TRIGGER rm_modifications_trigger
+    BEFORE INSERT ON rm
+    FOR EACH ROW
+/*    DECLARE 
+        newfid = NEW.fid;
+        newsrc = NEW.src;
+        newdst = NEW.dst; */
+    EXECUTE PROCEDURE check_rm_table();
+
+/* returns true if the insertion is allowed */
+CREATE OR REPLACE FUNCTION rm_boolean(newsrc varchar, newdst varchar)
+RETURNS BOOLEAN AS $rm_boolean$
+    SELECT (SELECT EXISTS( SELECT 1 FROM rm_trigger_src WHERE nodeid = CAST($1 AS integer) ) AND (SELECT EXISTS( SELECT 1 FROM rm_trigger_dst WHERE nodeid = CAST($2 AS integer) )) AND ( SELECT EXISTS(SELECT 1 FROM sla WHERE nodeid =  CAST($1 AS integer) AND name = current_user) OR (SELECT EXISTS(SELECT 1 FROM sla WHERE nodeid =  CAST($2 AS integer) AND name = current_user) )) OR (current_user = 'ravel') );
+$rm_boolean$ LANGUAGE SQL;
+
+/* trigger function */
+CREATE OR REPLACE FUNCTION check_rm_table()
+RETURNS TRIGGER
+AS $rm_modifications_trigger$
+BEGIN
+IF NOT rm_boolean(CAST(NEW.src AS varchar), CAST(NEW.dst AS varchar)) THEN
+    RETURN NULL;
+END IF;
+RETURN NEW;
+END
+$rm_modifications_trigger$ LANGUAGE 'plpgsql';
 
 /* current user's visible network traffic */
 CREATE VIEW rm_tenant AS (
@@ -78,42 +137,11 @@ CREATE VIEW rm_tenant AS (
 
 GRANT SELECT, INSERT, DELETE ON rm_tenant TO PUBLIC;
 
-/* veiws created because triggers do not support subqueries */
-CREATE VIEW rm_trigger_src AS (
-    SELECT nodeid FROM sla WHERE name IN (
-        SELECT p1 FROM config_sla WHERE p2=current_user )
-    UNION
-    SELECT nodeid FROM sla WHERE name = current_user );
-
-CREATE VIEW rm_trigger_dst AS (
-    SELECT nodeid FROM sla WHERE name IN (
-        SELECT p2 FROM config_sla WHERE p1=current_user )
-    UNION
-    SELECT nodeid FROM sla WHERE name = current_user );
-
-DROP TABLE IF EXISTS rm_exceptions;
-CREATE TABLE rm_exceptions ( name varchar );
-INSERT INTO rm_exceptions (name) VALUES ('ravel');
-
-/* trigger uses AFTER INSERT because WHEN is not supported for INSTEAD OF triggers */
-/* use before trigger (be more general for complicated cases/views that aren't updatabale,
-performance is better to avoid unnecessary actions 
-modify/override postgres modification commands */
-CREATE TRIGGER rm_modifications_trigger
-    INSTEAD OF INSERT ON rm_tenant
-    FOR EACH ROW
-    EXECUTE PROCEDURE check_rm_table( SELECT CAST ( NEW.fid AS text ), CAST(NEW.src AS text), CAST(NEW.dst AS text));
-
-/* write in plpy */
-CREATE OR REPLACE FUNCTION check_rm_table()
-RETURNS TRIGGER
-AS $rm_modifications_trigger$
-if plpy.execute("SELECT EXISTS( SELECT 1 FROM rm_trigger_src WHERE nodeid = " + TD['args'][1] + " ) AND (SELECT EXISTS( SELECT 1 FROM rm_trigger_dst WHERE nodeid = " + TD['args'][2] + " )) AND ( SELECT EXISTS(SELECT 1 FROM sla WHERE nodeid = " + TD['args'][1] + " AND name = current_user) OR (SELECT EXISTS(SELECT 1 FROM sla WHERE nodeid = " + TD['args'][2] + " AND name = current_user) ));"):
-    plpy.execute("INSERT INTO rm (fid, src, dst) VALUES (" + TD['args'][0] + ", " + TD['args'][1] + ", " + TD['args'][2] + ");")
-return None;
-$rm_modifications_trigger$ LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
-
 /* QUESTION: how does one restrict what public can insert? */
 /* TODO: restrict what current_user can insert to only flows that comply with conditions of view */
 /* TODO: make fid's automatically assigned, so that user gains as little info as possible about
         what/how many other flows there are */
+
+/* add some flows to the network */
+/* trigger is called on this insertion as well! */
+INSERT INTO rm (fid, src, dst) VALUES (1, 1, 5), (2, 4, 12), (3, 2, 17), (4, 7, 3), (5, 1, 13);
