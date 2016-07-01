f81304422e5ab3479273596256192040341289fe
nglaeser <nglaeser@email.sc.edu>
2016-07-01 13:38:54 -0500

    access control for insert into rm_tenant should work now

--- sla_start.sql
+++ sla_start.sql
@@ -31,9 +31,9 @@ UPDATE switches SET tenant = sla.name FROM sla
 
 /* each user's visible nodes */
 CREATE OR REPLACE VIEW topology_acl AS ( 
-    ( SELECT 'admin' AS principal, sid, nid FROM tp )
+    ( SELECT 'admin' AS principal, sid, nid, isactive FROM tp )
     UNION
-    ( SELECT s.name AS principal, sid, nid FROM tp, sla s
+    ( SELECT s.name AS principal, sid, nid, isactive FROM tp, sla s
         WHERE
         tp. sid IN (SELECT nodeid FROM sla WHERE name = s.name) AND
         tp. nid IN (SELECT nodeid FROM sla WHERE name = s.name) )
@@ -41,7 +41,7 @@ CREATE OR REPLACE VIEW topology_acl AS (
 
 /* current user's visible nodes (topo) */
 CREATE OR REPLACE VIEW topology_tenant AS (
-    SELECT sid, nid FROM topology_acl
+    SELECT sid, nid, isactive FROM topology_acl
     WHERE principal = current_user);
 
 GRANT SELECT ON topology_tenant TO PUBLIC;
@@ -56,7 +56,6 @@ DROP TABLE IF EXISTS config_sla CASCADE;
 /* whitelist for who can talk to whom */
 CREATE TABLE config_sla (p1 varchar, p2 varchar);
 INSERT INTO config_sla (p1, p2) VALUES ('alice', 'bob'), ('alice', 'charlie'), ('bob', 'alice'), ('charlie', 'alice');
-
 
 /* veiws created because triggers do not support subqueries */
 CREATE VIEW rm_trigger_src AS (
@@ -75,31 +74,6 @@ GRANT SELECT ON rm_trigger_dst TO PUBLIC;
 DROP TABLE IF EXISTS rm_exceptions;
 CREATE TABLE rm_exceptions ( name varchar );
 INSERT INTO rm_exceptions (name) VALUES ('ravel');
-
-/* trigger for modifications on rm */
-DROP TRIGGER IF EXISTS rm_modifications_trigger ON rm;
-CREATE TRIGGER rm_modifications_trigger
-    BEFORE INSERT ON rm
-    FOR EACH ROW
-    EXECUTE PROCEDURE check_rm_table();
-
-/* returns true if the insertion is allowed */
-CREATE OR REPLACE FUNCTION rm_boolean(newsrc varchar, newdst varchar)
-RETURNS BOOLEAN AS $rm_boolean$
-    SELECT (SELECT EXISTS( SELECT 1 FROM rm_trigger_src WHERE nodeid = CAST($1 AS integer) ) AND (SELECT EXISTS( SELECT 1 FROM rm_trigger_dst WHERE nodeid = CAST($2 AS integer) )) AND ( SELECT EXISTS(SELECT 1 FROM sla_tenant WHERE nodeid =  CAST($1 AS integer) ) OR (SELECT EXISTS(SELECT 1 FROM sla_tenant WHERE nodeid =  CAST($2 AS integer) ) )) OR (current_user = 'ravel') );
-$rm_boolean$ LANGUAGE SQL;
-
-/* trigger function */
-CREATE OR REPLACE FUNCTION check_rm_table()
-RETURNS TRIGGER
-AS $rm_modifications_trigger$
-BEGIN
-IF NOT rm_boolean(CAST(NEW.src AS varchar), CAST(NEW.dst AS varchar)) THEN
-    RETURN NULL;
-END IF;
-RETURN NEW;
-END
-$rm_modifications_trigger$ LANGUAGE 'plpgsql';
 
 /* current user's visible network traffic */
 CREATE VIEW rm_tenant AS (
@@ -123,6 +97,33 @@ CREATE VIEW rm_tenant AS (
 );
 
 GRANT SELECT, INSERT, DELETE ON rm_tenant TO PUBLIC;
+
+/* trigger for modifications on rm */
+DROP TRIGGER IF EXISTS rm_modifications_trigger ON rm_tenant;
+CREATE TRIGGER rm_modifications_trigger
+    BEFORE INSERT ON rm
+    FOR EACH ROW
+    EXECUTE PROCEDURE check_rm_table();
+
+/* returns true if the insertion is allowed */
+CREATE OR REPLACE FUNCTION rm_boolean(newsrc varchar, newdst varchar)
+RETURNS BOOLEAN AS $rm_boolean$
+    SELECT (SELECT EXISTS( SELECT 1 FROM rm_trigger_src WHERE nodeid = CAST($1 AS integer) ) AND (SELECT EXISTS( SELECT 1 FROM rm_trigger_dst WHERE nodeid = CAST($2 AS integer) )) AND ( SELECT EXISTS(SELECT 1 FROM sla_tenant WHERE nodeid =  CAST($1 AS integer) ) OR (SELECT EXISTS(SELECT 1 FROM sla_tenant WHERE nodeid =  CAST($2 AS integer) ) )) OR (current_user = 'ravel') );
+$rm_boolean$ LANGUAGE SQL;
+
+/* trigger function */
+/* returns NULL (i.e. no insertion done) if flow disallowed
+   else, inserts the flow as requested */
+CREATE OR REPLACE FUNCTION check_rm_table()
+RETURNS TRIGGER
+AS $rm_modifications_trigger$
+BEGIN
+IF NOT rm_boolean(CAST(NEW.src AS varchar), CAST(NEW.dst AS varchar)) THEN
+    RETURN NULL;
+END IF;
+RETURN NEW;
+END
+$rm_modifications_trigger$ LANGUAGE 'plpgsql';
 
 /* QUESTION: how does one restrict what public can insert? */
 /* TODO: restrict insertions into rm_tenant to only flows where the source is in the current_user's area of the network? */
