1b06e41da40c06edbc442690f8637e3f2c4c1016
nglaeser <nglaeser@email.sc.edu>
2016-09-19 21:27:21 -0500

    restore all functionality after Jason's bug fixes to Ravel

--- acl_demo.sh
+++ acl_demo.sh
@@ -14,15 +14,24 @@ acl addtenant alice 1 2 3 11 12 13
 acl addtenant bob 4 5 14 15
 
 acl whitelist alice bob
-# acl whitelist alice charlie
+acl whitelist alice charlie
 acl whitelist bob alice
-# acl whitelist charlie alice
+acl whitelist charlie alice
 
-# rt addflow h1 h5
-# rt addflow h4 h2
-# rt addflow h2 h7
-# rt addflow h1 h3
-# rt addflow h7 h3
+# to test dijkstra makes path only through alice
+# and bob's area
+# shortest path = 1-2-6-5
+# shortest with restriction = 1-2-3-4-5
+rt addflow h1 h5 # alice to bob
 
-# rt addflow h3 h4
+rt addflow h4 h2 # bob to alice
+
+rt addflow h2 h7 # alice to charlie
+
+rt addflow h1 h3 # alice to alice
+
+rt addflow h7 h3 # charlie to alice
+
+rt addflow h3 h4 # alice to bob
+
 # orch run
